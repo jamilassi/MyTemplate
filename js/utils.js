6c8d341ebbe938709689b146c90669b7ae03e688
jamil- <Jamil.Assi.email@gmail.com>
2015-01-29 11:04:35 -0500

    Initial Commit
    Skeleton with no deep descriptions,
    have to be changed with good descriptions.

---
+++ js/utils.js
@@ -0,0 +1,31 @@
+/* JavaScript Document 
+
+utils.js Personal Library
+Jamil Assi
+*/
+(function(context){
+
+context.Utils = {};
+
+function show(id) {
+	var ele=document.getElementById(id);
+	if (ele.style.display === "none")
+	{
+		ele.style.display="block";
+	}
+	else {
+		ele.style.display="none";
+		}
+	}
+
+context.Utils.show = show;
+
+function hide(el) {
+	el.style.display="none";
+	}
+
+context.Utils.hide = hide;
+
+
+
+})(window);
